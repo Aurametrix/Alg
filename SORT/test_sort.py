8228509a2a373d2787343ead75dbd689a534f424
Irene <irene@aurametrix.com>
2013-08-17 10:24:25 -0700

    Insertion sort

--- SORT/test_sort.py
+++ SORT/test_sort.py
@@ -1,8 +1,10 @@
 if __name__=="__main__":                      
     import sys
-    from quicksort import quicksort
+    #from quicksort import quicksort
+    from insertionsort import insertionsort
     lst = map(int,sys.argv[1:])        # Get typed numbers as list
     #print quicksort(lst)          # print out the sorted list as a list
+    print insertionsort(lst)
     import string
-    print string.join(map(str,quicksort(lst) ))    # Print as a string 
-    
+    #print string.join(map(str,quicksort(lst) ))    # Print as a string 
+    print string.join(map(str,insertionsort(lst) ))    # Print as a string 
