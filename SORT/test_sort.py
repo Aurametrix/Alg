98f7f221e48be1adf89350b7381823d63cc271db
Irene <irene@aurametrix.com>
2013-08-23 07:48:54 -0700

    added a checker for arguments

--- SORT/test_sort.py
+++ SORT/test_sort.py
@@ -2,6 +2,11 @@ if __name__=="__main__":
     import sys
     #from quicksort import quicksort
     from insertionsort import insertionsort
+
+    if (len(sys.argv) < 2):
+        print "please tyoe a few numbers after test_sort.py and I will sort them" 
+        #print 'Argument List:', str(sys.argv)
+
     lst = map(int,sys.argv[1:])        # Get typed numbers as list
     #print quicksort(lst)          # print out the sorted list as a list
     print insertionsort(lst)
