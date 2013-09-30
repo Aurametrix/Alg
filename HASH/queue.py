b732ed81628b0e4fdc4cb692fd312a66dc6d146e
Irene <irene@aurametrix.com>
2013-09-29 20:29:00 -0700

    using Deque

--- HASH/queue.py
+++ HASH/queue.py
@@ -12,3 +12,14 @@ print "after popping the first element", a
 a.insert(0, -1)
 print "after inserting ", a
 
+# using a deque is much faster.
+from collections import deque
+queue = deque([66.25, 333, 333, 1, 1234.5])
+queue.append(-1)           # add -1
+print "after inserting ", queue
+queue.append("First")          # First arrives
+print "after inserting ", queue
+queue.popleft()                 # The first to arrive now leaves
+print "after popleft ", queue
+queue.popleft()                 # The second to arrive now leaves
+print "after popleft ", queue
