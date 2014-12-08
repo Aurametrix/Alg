c69586ad8eadc972cd1799dfcdf910cc9352eef1
Irene <irene@aurametrix.com>
2014-12-07 18:22:57 -0800

    concatenate multiple files

---
+++ REGEX/concatenate.py
@@ -0,0 +1,20 @@
+# to check file size
+import os
+statinfo = os.stat('file1.txt')
+statinfo
+statinfo.st_size
+
+# for large files
+filenames = ['file1.txt', 'file2.txt', ...]
+with open('path/to/output/file', 'w') as outfile:
+    for fname in filenames:
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(line)
+
+# for small files
+filenames = ['file1.txt', 'file2.txt', ...]
+with open('path/to/output/file', 'w') as outfile:
+    for fname in filenames:
+        with open(fname) as infile:
+            outfile.write(infile.read())
