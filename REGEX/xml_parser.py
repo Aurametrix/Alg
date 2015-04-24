3c9460fb7c30c279b5927971a5f236dc187f8590
Irene <irene@aurametrix.com>
2015-04-23 20:25:28 -0700

    finding interesting leements

--- REGEX/xml_parser.py
+++ REGEX/xml_parser.py
@@ -17,3 +17,22 @@ print "3rd sub-element of the second element:", root[1][2].text
 
 #for atype in e.findall('type'):
 #    print(atype.get('foobar'))
+
+
+#finding interesting elements
+#Element.iter() is a method that helps iterate recursively over all the sub-trees below it - children, their children, etc 
+
+for neighbor in root.iter('neighbor'):
+   print neighbor.attrib
+
+# Element.findall() finds only elements with a tag which are direct children of the current element. 
+# Element.find() finds the first child with a particular tag
+# Element.text accesses the element's text content. 
+# Element.get() accesses the element's attributes
+
+for country in root.findall('country'):
+    rank = country.find('rank').text
+    name = country.get('name')
+    print name, rank
+
+
