c7ba4153517e9abcf60daea504b29679472b6dcf
Aurametrix <irene@aurametrix.com>
2015-08-04 16:55:51 -0700

    adding recursive function

--- REGEX/palindrome.py
+++ REGEX/palindrome.py
@@ -1,2 +1,10 @@
 def is_palindrome(s):
   return s == s[::-1]
+
+def is_palindrome_r(s):
+  if len(s) <= 1:
+    return True
+  elif s[0] != s[-1]:
+    return False
+  else:
+    return is_palindrome_r(s[1:-1])
