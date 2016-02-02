f4b7214a306a7921cad5a5b6cc4eb932f6d4d3cd
Aurametrix <irene@aurametrix.com>
2016-02-01 17:57:53 -0800

    classifier without optimization

--- BIOMED/cssscl/build_databases.py
+++ BIOMED/cssscl/build_databases.py
@@ -1,1 +1,5 @@
  cssscl build_dbs -btax -c -blast -nt 2 PATH_TO/test_data/TRAIN.fa PATH_TO/taxon/
+ 
+ 
+#  use cssscl to classify sequences in a test set  TEST.fa
+cssscl classify -c -blast blastn -tax genus -nt 2 PATH_TO/test_data/test/TEST.fa PATH_TO/test_data/
