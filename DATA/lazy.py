e07ec12c4a7fd42de59dab9addc1d82eac2c933e
Aurametrix <irene@aurametrix.com>
2016-02-07 19:02:18 -0800

    simple data loading script using DATASET
    https://github.com/pudo/dataset
    code without dataset -- https://gist.github.com/gka/5296492
    https://dataset.readthedocs.org/en/latest/

---
+++ DATA/lazy.py
@@ -0,0 +1,9 @@
+import dataset
+
+db = dataset.connect('sqlite:///:memory:')
+
+table = db['sometable']
+table.insert(dict(name='John Doe', age=37))
+table.insert(dict(name='Jane Doe', age=34, gender='female'))
+
+john = table.find_one(name='John Doe')
