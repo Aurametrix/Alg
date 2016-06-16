9daea3327e41199c717cb6eb2e34a58de73a8306
Aurametrix <irene@aurametrix.com>
2016-06-15 17:59:08 -0700

    deprecated Google Image Search API, cStringIO and simplejson

--- WEB/g-imgs.py
+++ WEB/g-imgs.py
@@ -1,10 +1,12 @@
 import urllib2
-import simplejson
-import cStringIO
+import json as simplejson
+import io as cStringIO
 
 fetcher = urllib2.build_opener()
 searchTerm = 'Aurametrix'
 startIndex = 0
 searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + searchTerm + "&start=" + startIndex
 f = fetcher.open(searchUrl)
+searchUrl = ('https://ajax.googleapis.com/ajax/services/search/images?' +
+       'v=1.0&q=barack%20obama&userip=INSERT-USER-IP')
 deserialized_output = simplejson.load(f)
