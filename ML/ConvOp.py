7b22e892ebe898b8ebeffb7325f718aab9418b8a
Aurametrix <irene@aurametrix.com>
2016-06-16 21:17:19 -0700

    implementing convolutional layer
    http://deeplearning.net/tutorial/lenet.html

---
+++ ML/ConvOp.py
@@ -0,0 +1,60 @@
+import theano
+from theano import tensor as T
+from theano.tensor.nnet import conv2d
+
+import numpy
+
+rng = numpy.random.RandomState(23455)
+
+# instantiate 4D tensor for input
+input = T.tensor4(name='input')
+
+# initialize shared variable for weights.
+w_shp = (2, 3, 9, 9)
+w_bound = numpy.sqrt(3 * 9 * 9)
+W = theano.shared( numpy.asarray(
+            rng.uniform(
+                low=-1.0 / w_bound,
+                high=1.0 / w_bound,
+                size=w_shp),
+            dtype=input.dtype), name ='W')
+
+# initialize shared variable for bias (1D tensor) with random values
+# IMPORTANT: biases are usually initialized to zero. However in this
+# particular application, we simply apply the convolutional layer to
+# an image without learning the parameters. We therefore initialize
+# them to random values to "simulate" learning.
+b_shp = (2,)
+b = theano.shared(numpy.asarray(
+            rng.uniform(low=-.5, high=.5, size=b_shp),
+            dtype=input.dtype), name ='b')
+
+# build symbolic expression that computes the convolution of input with filters in w
+conv_out = conv2d(input, W)
+
+# build symbolic expression to add bias and apply activation function, i.e. produce neural net layer output
+# A few words on ``dimshuffle`` :
+#   ``dimshuffle`` is a powerful tool in reshaping a tensor;
+#   what it allows you to do is to shuffle dimension around
+#   but also to insert new ones along which the tensor will be
+#   broadcastable;
+#   dimshuffle('x', 2, 'x', 0, 1)
+#   This will work on 3d tensors with no broadcastable
+#   dimensions. The first dimension will be broadcastable,
+#   then we will have the third dimension of the input tensor as
+#   the second of the resulting tensor, etc. If the tensor has
+#   shape (20, 30, 40), the resulting tensor will have dimensions
+#   (1, 40, 1, 20, 30). (AxBxC tensor is mapped to 1xCx1xAxB tensor)
+#   More examples:
+#    dimshuffle('x') -> make a 0d (scalar) into a 1d vector
+#    dimshuffle(0, 1) -> identity
+#    dimshuffle(1, 0) -> inverts the first and second dimensions
+#    dimshuffle('x', 0) -> make a row out of a 1d vector (N to 1xN)
+#    dimshuffle(0, 'x') -> make a column out of a 1d vector (N to Nx1)
+#    dimshuffle(2, 0, 1) -> AxBxC to CxAxB
+#    dimshuffle(0, 'x', 1) -> AxB to Ax1xB
+#    dimshuffle(1, 'x', 0) -> AxB to Bx1xA
+output = T.nnet.sigmoid(conv_out + b.dimshuffle('x', 0, 'x', 'x'))
+
+# create theano function to compute filtered images
+f = theano.function([input], output)
