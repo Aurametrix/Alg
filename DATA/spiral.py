747721c43fda0a7b93cdd4895150e9e40f42200e
Aurametrix <irene@aurametrix.com>
2016-07-01 21:02:41 -0700

    generating toy spiral data consisting of three classes (blue, red, yellow) that are not linearly separable
    Stanford CS class CS231n: Convolutional Neural Networks for Visual Recognition
    http://cs231n.github.io/neural-networks-case-study/

---
+++ DATA/spiral.py
@@ -0,0 +1,13 @@
+N = 100 # number of points per class
+D = 2 # dimensionality
+K = 3 # number of classes
+X = np.zeros((N*K,D)) # data matrix (each row = single example)
+y = np.zeros(N*K, dtype='uint8') # class labels
+for j in xrange(K):
+  ix = range(N*j,N*(j+1))
+  r = np.linspace(0.0,1,N) # radius
+  t = np.linspace(j*4,(j+1)*4,N) + np.random.randn(N)*0.2 # theta
+  X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
+  y[ix] = j
+# lets visualize the data:
+plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
