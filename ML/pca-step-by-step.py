1bf3b4466bc384bcb68b4b64624ecfe5d771f194
Aurametrix <irene@aurametrix.com>
2017-02-06 17:50:39 -0800

    dropping one dimension
    http://sebastianraschka.com/Articles/2014_pca_step_by_step.html
    https://news.ycombinator.com/item?id=13584379
    
    https://arxiv.org/abs/1701.00858
    http://michaeljflynn.net/2017/02/06/a-tutorial-on-principal-component-analysis/

---
+++ ML/pca-step-by-step.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+np.random.seed(234234782384239784) # random seed for consistency
+
+# A reader pointed out that Python 2.7 would raise a
+# "ValueError: object of too small depth for desired array".
+# This can be avoided by choosing a smaller random seed, e.g. 1
+# or by completely omitting this line, since I just used the random seed for
+# consistency.
+
+mu_vec1 = np.array([0,0,0])
+cov_mat1 = np.array([[1,0,0],[0,1,0],[0,0,1]])
+class1_sample = np.random.multivariate_normal(mu_vec1, cov_mat1, 20).T
+assert class1_sample.shape == (3,20), "The matrix has not the dimensions 3x20"
+
+mu_vec2 = np.array([1,1,1])
+cov_mat2 = np.array([[1,0,0],[0,1,0],[0,0,1]])
+class2_sample = np.random.multivariate_normal(mu_vec2, cov_mat2, 20).T
+assert class1_sample.shape == (3,20), "The matrix has not the dimensions 3x20"
