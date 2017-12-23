507dba760984db095a34fc64d85a7d9ba6f3d309
Aurametrix <irene@aurametrix.com>
2017-12-23 12:28:38 -0800

    traffic sign toy application
    https://chsasank.github.io/keras-tutorial.html
    https://news.ycombinator.com/item?id=15993852
    
    see also - noise suppression: https://people.xiph.org/~jm/demo/rnnoise/

--- ML/keras.py
+++ ML/keras.py
@@ -1,7 +1,56 @@
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.utils.vis_utils import plot_model
 model = Sequential()
 model.add(Dense(2, input_dim=1, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+
+
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.optimizers import SGD
+from keras import backend as K
+K.set_image_data_format('channels_first')
+
+# what parameters mean  https://keras.io/layers/convolutional/
+# for different layers  https://keras.io/layers/core/
+
+def cnn_model():
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), padding='same',
+                     input_shape=(3, IMG_SIZE, IMG_SIZE),
+                     activation='relu'))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(64, (3, 3), padding='same',
+                     activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(128, (3, 3), padding='same',
+                     activation='relu'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
+    return model
+  
+  from keras.optimizers import SGD
+
+model = cnn_model()
+
+# let's train the model using SGD + momentum
+lr = 0.01
+sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
