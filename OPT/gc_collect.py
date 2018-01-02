31b6a83b09acab22c14d9bf75bf5a63232f8138f
Aurametrix <irene@aurametrix.com>
2018-01-01 21:25:36 -0800

    forcing garbage collection
    If you create a large object and delete it again, Python has probably released the memory, but the memory allocators involved don’t necessarily return the memory to the operating system, so it may look as if the Python process uses a lot more virtual memory than it actually uses.
    The only really reliable way to ensure that a large but temporary use of memory DOES return all resources to the system when it's done, is to have that use happen in a subprocess, which does the memory-hungry work then terminates.
    
    https://pymotw.com/2/gc/
    https://pymotw.com/3/gc/
    https://www.youtube.com/watch?v=hg01TTzi6Xk
    	
    Python will automatically collect objects with circular references, unless any of the objects in a reference cycle have del methods. If that's the case, garbage objects are moved to the gc.garbage list, and you will have to manually break the reference cycles. It's better to try to avoid having both del methods and reference cycles.

---
+++ OPT/gc_collect.py
@@ -0,0 +1,19 @@
+
+import gc
+import pprint
+
+
+class Graph:
+
+    def __init__(self, name):
+        self.name = name
+        self.next = None
+
+    def set_next(self, next):
+        print('Linking nodes {}.next = {}'.format(self, next))
+        self.next = next
+
+    def __repr__(self):
+        return '{}({})'.format(
+            self.__class__.__name__, self.name)
+            
