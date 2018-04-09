734d5d9c579a06fd280cb4e5aa412672dcca526b
Aurametrix <irene@aurametrix.com>
2018-04-09 13:57:19 -0700

    python interpreter
    http://aosabook.org/en/500L/a-python-interpreter-written-in-python.html
    https://news.ycombinator.com/item?id=16795049

---
+++ OPT/interpreter.py
@@ -0,0 +1,41 @@
+class Interpreter:
+    def __init__(self):
+        self.stack = []
+        self.environment = {}
+
+    def STORE_NAME(self, name):
+        val = self.stack.pop()
+        self.environment[name] = val
+
+    def LOAD_NAME(self, name):
+        val = self.environment[name]
+        self.stack.append(val)
+
+    def parse_argument(self, instruction, argument, what_to_execute):
+        """ Understand what the argument to each instruction means."""
+        numbers = ["LOAD_VALUE"]
+        names = ["LOAD_NAME", "STORE_NAME"]
+
+        if instruction in numbers:
+            argument = what_to_execute["numbers"][argument]
+        elif instruction in names:
+            argument = what_to_execute["names"][argument]
+
+        return argument
+
+    def run_code(self, what_to_execute):
+        instructions = what_to_execute["instructions"]
+        for each_step in instructions:
+            instruction, argument = each_step
+            argument = self.parse_argument(instruction, argument, what_to_execute)
+
+            if instruction == "LOAD_VALUE":
+                self.LOAD_VALUE(argument)
+            elif instruction == "ADD_TWO_VALUES":
+                self.ADD_TWO_VALUES()
+            elif instruction == "PRINT_ANSWER":
+                self.PRINT_ANSWER()
+            elif instruction == "STORE_NAME":
+                self.STORE_NAME(argument)
+            elif instruction == "LOAD_NAME":
+                self.LOAD_NAME(argument)
