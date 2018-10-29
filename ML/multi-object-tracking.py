53534162b1d2176fc8f97b492a07c09bf07e02c0
Aurametrix <irene@aurametrix.com>
2018-10-29 13:01:03 -0400

    Multi-object tracking with dlib
    e-mail solicitation
    https://www.pyimagesearch.com/2018/10/29/multi-object-tracking-with-dlib/

---
+++ ML/multi-object-tracking.py
@@ -0,0 +1,35 @@
+# import the necessary packages
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import dlib
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-v", "--video", required=True,
+	help="path to input video file")
+ap.add_argument("-o", "--output", type=str,
+	help="path to optional output video file")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-v", "--video", required=True,
+	help="path to input video file")
+ap.add_argument("-o", "--output", type=str,
+	help="path to optional output video file")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
