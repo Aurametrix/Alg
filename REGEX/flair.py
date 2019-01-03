f311fa16dd7ebd771607b7fecf720b565b97cd8a
Aurametrix <irene@aurametrix.com>
2019-01-02 22:45:46 -0500

    NLP framework
    https://github.com/zalandoresearch/flair
    https://news.ycombinator.com/item?id=18804639

---
+++ REGEX/flair.py
@@ -0,0 +1,11 @@
+from flair.data import Sentence
+from flair.models import SequenceTagger
+
+# make a sentence
+sentence = Sentence('I love Berlin .')
+
+# load the NER tagger
+tagger = SequenceTagger.load('ner')
+
+# run NER over sentence
+tagger.predict(sentence)
