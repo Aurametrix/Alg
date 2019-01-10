5bd29f3c68f508205a088bb1c46c0cbb75d3b65f
Aurametrix <irene@aurametrix.com>
2019-01-09 19:09:03 -0500

    Analyze a Soccer game using Tensorflow Object Detection and OpenCV
    https://news.ycombinator.com/item?id=18865265
    https://towardsdatascience.com/analyse-a-soccer-game-using-tensorflow-object-detection-and-opencv-e321c230e8f2
    https://github.com/priya-dwivedi/Deep-Learning/blob/master/soccer_team_prediction/soccer_realtime.ipynb

---
+++ ML/soccer.py
@@ -0,0 +1,13 @@
+from __future__ import division
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from collections import defaultdict
+from io import StringIO
+from PIL import Image
+import cv2
