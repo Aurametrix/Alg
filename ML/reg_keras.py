4745611ca7c6e2f95ad82a2d6eae24920953c14a
Aurametrix <irene@aurametrix.com>
2019-01-22 21:41:49 -0500

    Regression with Keras
    https://www.pyimagesearch.com/2019/01/21/regression-with-keras/

---
+++ ML/reg_keras.py
@@ -0,0 +1,13 @@
+# one-hot encode the zip code categorical data (by definition of
+	# one-hot encoing, all output features are now in the range [0, 1])
+	zipBinarizer = LabelBinarizer().fit(df["zipcode"])
+	trainCategorical = zipBinarizer.transform(train["zipcode"])
+	testCategorical = zipBinarizer.transform(test["zipcode"])
+ 
+	# construct our training and testing data points by concatenating
+	# the categorical features with the continuous features
+	trainX = np.hstack([trainCategorical, trainContinuous])
+	testX = np.hstack([testCategorical, testContinuous])
+ 
+	# return the concatenated training and testing data
+	return (trainX, testX)
