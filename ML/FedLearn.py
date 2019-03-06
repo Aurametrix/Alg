7ee76f7972b2f8308a25f97792597b42921496ab
Aurametrix <irene@aurametrix.com>
2019-03-05 23:03:46 -0500

    Federated learning
    https://blog.openmined.org/upgrade-to-federated-learning-in-10-lines/

---
+++ ML/FedLearn.py
@@ -0,0 +1,4 @@
+import syft as sy  # <-- NEW: import the Pysyft library
+hook = sy.TorchHook(torch)  # <-- NEW: hook PyTorch ie add extra functionalities to support Federated Learning
+bob = sy.VirtualWorker(hook, id="bob")  # <-- NEW: define remote worker bob
+alice = sy.VirtualWorker(hook, id="alice")  # <-- NEW: and alice
