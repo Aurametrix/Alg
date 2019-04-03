08409037599b64aea415c9270381217bfba484f6
Aurametrix <irene@aurametrix.com>
2019-04-02 20:47:39 -0400

    RNN
    https://www.cpuheater.com/deep-learning/introduction-to-recurrent-neural-networks-in-pytorch/
    https://github.com/cpuheater/pytorch_examples
    https://news.ycombinator.com/item?id=16587291

---
+++ ML/rnn_pytorch.py
@@ -0,0 +1,66 @@
+import torch
+from torch.autograd import Variable
+import numpy as np
+import pylab as pl
+import torch.nn.init as init
+
+dtype = torch.FloatTensor
+input_size, hidden_size, output_size = 7, 6, 1
+epochs = 300
+seq_length = 20
+lr = 0.1
+
+
+data_time_steps = np.linspace(2, 10, seq_length + 1)
+data = np.sin(data_time_steps)
+data.resize((seq_length + 1, 1))
+
+x = Variable(torch.Tensor(data[:-1]).type(dtype), requires_grad=False)
+y = Variable(torch.Tensor(data[1:]).type(dtype), requires_grad=False)
+
+data_time_steps = np.linspace(2, 10, seq_length + 1)
+data = np.sin(data_time_steps)
+data.resize((seq_length + 1, 1))
+ 
+x = Variable(torch.Tensor(data[:-1]).type(dtype), requires_grad=False)
+y = Variable(torch.Tensor(data[1:]).type(dtype), requires_grad=False)
+
+w1 = torch.FloatTensor(input_size, hidden_size).type(dtype)
+init.normal(w1, 0.0, 0.4)
+w1 =  Variable(w1, requires_grad=True)
+w2 = torch.FloatTensor(hidden_size, output_size).type(dtype)
+init.normal(w2, 0.0, 0.3)
+w2 = Variable(w2, requires_grad=True)
+
+def forward(input, context_state, w1, w2):
+  xh = torch.cat((input, context_state), 1)
+  context_state = torch.tanh(xh.mm(w1))
+  out = context_state.mm(w2)
+  return  (out, context_state)
+  
+for i in range(epochs):
+  total_loss = 0
+  context_state = Variable(torch.zeros((1, hidden_size)).type(dtype), requires_grad=True)
+  for j in range(x.size(0)):
+    input = x[j:(j+1)]
+    target = y[j:(j+1)]
+    (pred, context_state) = forward(input, context_state, w1, w2)
+    loss = (pred - target).pow(2).sum()/2
+    total_loss += loss
+    loss.backward()
+    w1.data -= lr * w1.grad.data
+    w2.data -= lr * w2.grad.data
+    w1.grad.data.zero_()
+    w2.grad.data.zero_()
+    context_state = Variable(context_state.data)
+  if i % 10 == 0:
+     print("Epoch: {} loss {}".format(i, total_loss.data[0]))
+     
+     context_state = Variable(torch.zeros((1, hidden_size)).type(dtype), requires_grad=False)
+predictions = []
+ 
+for i in range(x.size(0)):
+  input = x[i:i+1]
+  (pred, context_state) = forward(input, context_state, w1, w2)
+  context_state = context_state
+  predictions.append(pred.data.numpy().ravel()[0])
