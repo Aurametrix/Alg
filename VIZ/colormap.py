05415947c3221b99c527de85e6a4d990102eed3e
Aurametrix <irene@aurametrix.com>
2019-04-12 20:35:28 -0400

    Colormap for Matplotlib
    https://jakevdp.github.io/blog/2014/10/16/how-bad-is-your-colormap/
    https://news.ycombinator.com/item?id=19642379

---
+++ VIZ/colormap.py
@@ -0,0 +1,12 @@
+def grayify_cmap(cmap):
+    """Return a grayscale version of the colormap"""
+    cmap = plt.cm.get_cmap(cmap)
+    colors = cmap(np.arange(cmap.N))
+    
+    # convert RGBA to perceived greyscale luminance
+    # cf. http://alienryderflex.com/hsp.html
+    RGB_weight = [0.299, 0.587, 0.114]
+    luminance = np.sqrt(np.dot(colors[:, :3] ** 2, RGB_weight))
+    colors[:, :3] = luminance[:, np.newaxis]
+    
+    return cmap.from_list(cmap.name + "_grayscale", colors, cmap.N)
