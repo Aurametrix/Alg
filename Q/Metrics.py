0c9fd50dd8e9e21bafc3cc77f70e2dc76301f1ca
Aurametrix <irene@aurametrix.com>
2019-06-17 21:42:37 -0400

    pointers

--- Q/Metrics.py
+++ Q/Metrics.py
@@ -4,3 +4,17 @@ class Metrics(object):
             "func_calls": 0,
             "cat_pictures_served": 0,
         }
+
+    @property
+    def func_calls(self):
+        return self._metrics["func_calls"]
+
+    @property
+    def cat_pictures_served(self):
+        return self._metrics["cat_pictures_served"]
+
+    def inc_func_calls(self):
+        self._metrics["func_calls"] += 1
+
+    def inc_cat_pics(self):
+        self._metrics["cat_pictures_served"] += 1
