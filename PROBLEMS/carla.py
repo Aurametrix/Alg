7f3d7c3ab1a759636d9fae2b9794c126a89870b7
Aurametrix <irene@aurametrix.com>
2019-08-09 22:02:01 -0400

    simulator for autonomous driving research
    https://github.com/carla-simulator/carla
    https://carla.readthedocs.io/en/latest/python_api_tutorial/
    https://github.com/carla-simulator
    http://carla.org/
    https://news.ycombinator.com/item?id=20657366

---
+++ PROBLEMS/carla.py
@@ -0,0 +1,15 @@
+carla.VehiclePhysicsControl(
+    torque_curve,
+    max_rpm,
+    moi,
+    damping_rate_full_throttle,
+    damping_rate_zero_throttle_clutch_engaged,
+    damping_rate_zero_throttle_clutch_disengaged,
+    use_gear_autobox,
+    gear_switch_time,
+    clutch_strength,
+    mass,
+    drag_coefficient,
+    center_of_mass,
+    steering_curve,
+    wheels)
