9eb99c9f3e883cb652b7138bc1127dfbb212cf05
Aurametrix <irene@aurametrix.com>
2019-08-12 19:41:03 -0400

    AWS
    This workflow will create buckets and an API to manage Video On Demand content with AWS.
    
    
    AWS workflow https://github.com/trackit/aws-workflow-video-on-demand
    https://news.ycombinator.com/item?id=20681269

---
+++ OPT/aws_config.py
@@ -0,0 +1,38 @@
+import os
+import json
+
+path = './config.json'
+
+conf = None
+
+def read_conf():
+    global conf
+    if conf is None:
+        if os.path.isfile(path):
+            with open(path) as raw:
+                conf = json.load(raw)
+                return conf
+        else:
+            return None
+    return conf
+
+def get_config(*args):
+    data = read_conf()
+    for arg in args:
+        if arg in data:
+            data = data[arg]
+        else:
+            return None
+    return data
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
