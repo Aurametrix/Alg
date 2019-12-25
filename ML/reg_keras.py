b0e5e594c33cefccafe370dabd476477efa0e175
Aurametrix <irene@aurametrix.com>
2019-12-24 21:46:12 -0500

    with houses dataset
    https://www.pyimagesearch.com/2019/01/21/regression-with-keras/

--- ML/reg_keras.py
+++ ML/reg_keras.py
@@ -1,4 +1,70 @@
+# import the necessary packages
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import numpy as np
+import glob
+import cv2
+import os
+ 
+def load_house_attributes(inputPath):
+	# initialize the list of column names in the CSV file and then
+	# load it using Pandas
+	cols = ["bedrooms", "bathrooms", "area", "zipcode", "price"]
+	df = pd.read_csv(inputPath, sep=" ", header=None, names=cols)
+	
+
+#   bedrooms  bathrooms  area  zipcode     price
+#0         4        4.0  4053    85255  869500.0
+#1         4        3.0  3343    36372  865200.0	
+
+# determine (1) the unique zip codes and (2) the number of data
+# points with each zip code
+	zipcodes = df["zipcode"].value_counts().keys().tolist()
+	counts = df["zipcode"].value_counts().tolist()
+ 
+# loop over each of the unique zip codes and their corresponding
+# count
+	for (zipcode, count) in zip(zipcodes, counts):
+		# the zip code counts for our housing dataset is *extremely*
+		# unbalanced (some only having 1 or 2 houses per zip code)
+		# so let's sanitize our data by removing any houses with less
+		# than 25 houses per zip code
+		if count < 25:
+			idxs = df[df["zipcode"] == zipcode].index
+			df.drop(idxs, inplace=True)
+ 
+# return the data frame
+	return df
+	
 # one-hot encode the zip code categorical data (by definition of
+# one-hot encoing, all output features are now in the range [0, 1])
+#	zipBinarizer = LabelBinarizer().fit(df["zipcode"])
+#	trainCategorical = zipBinarizer.transform(train["zipcode"])
+#	testCategorical = zipBinarizer.transform(test["zipcode"])
+ 
+# construct our training and testing data points by concatenating
+# the categorical features with the continuous features
+#	trainX = np.hstack([trainCategorical, trainContinuous])
+#	testX = np.hstack([testCategorical, testContinuous])
+ 
+# return the concatenated training and testing data
+#	return (trainX, testX)
+
+
+# preprocess data
+def process_house_attributes(df, train, test):
+	# initialize the column names of the continuous data
+	continuous = ["bedrooms", "bathrooms", "area"]
+ 
+	# performin min-max scaling each continuous feature column to
+	# the range [0, 1]
+	cs = MinMaxScaler()
+	trainContinuous = cs.fit_transform(train[continuous])
+	testContinuous = cs.transform(test[continuous])
+	
+# preprocess categorical features
+	# one-hot encode the zip code categorical data (by definition of
 	# one-hot encoing, all output features are now in the range [0, 1])
 	zipBinarizer = LabelBinarizer().fit(df["zipcode"])
 	trainCategorical = zipBinarizer.transform(train["zipcode"])
