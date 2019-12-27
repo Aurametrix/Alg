59be01e09444bf19dd443ccf74ede86875799535
Aurametrix <irene@aurametrix.com>
2019-12-26 21:22:49 -0500

    Using Neo4j from Python
    https://neo4j.com/developer/python/

---
+++ GRAPH/neo4j.py
@@ -0,0 +1,21 @@
+from neo4j import GraphDatabase
+
+class HelloWorldExample(object):
+
+    def __init__(self, uri, user, password):
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self._driver.close()
+
+    def print_greeting(self, message):
+        with self._driver.session() as session:
+            greeting = session.write_transaction(self._create_and_return_greeting, message)
+            print(greeting)
+            
+    @staticmethod
+    def _create_and_return_greeting(tx, message):
+        result = tx.run("CREATE (a:Greeting) "
+                        "SET a.message = $message "
+                        "RETURN a.message + ', from node ' + id(a)", message=message)
+        return result.single()[0]
