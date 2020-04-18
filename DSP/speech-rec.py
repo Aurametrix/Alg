e11a97172a952f969f489038b715550233a6793c
Aurametrix <irene@aurametrix.com>
2020-04-17 22:16:20 -0400

    end-to-end Speech Recognition model in PyTorch
    https://colab.research.google.com/drive/1IPpwx4rX32rqHKpLz7dc8sOKspUa-YKO
    https://www.assemblyai.com/blog/end-to-end-speech-recognition-pytorch
    https://news.ycombinator.com/item?id=22899107

---
+++ DSP/speech-rec.py
@@ -0,0 +1,24 @@
+import os
+from comet_ml import Experiment
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+import torch.optim as optim
+import torch.nn.functional as F
+import torchaudio
+import numpy as np
+
+def avg_wer(wer_scores, combined_ref_len):
+    return float(sum(wer_scores)) / float(combined_ref_len)
+
+
+def _levenshtein_distance(ref, hyp):
+    """Levenshtein distance is a string metric for measuring the difference
+    between two sequences. Informally, the levenshtein disctance is defined as
+    the minimum number of single-character edits (substitutions, insertions or
+    deletions) required to change one word into the other. We can naturally
+    extend the edits to word level when calculate levenshtein disctance for
+    two sentences.
+    """
+    m = len(ref)
+    n = len(hyp)
