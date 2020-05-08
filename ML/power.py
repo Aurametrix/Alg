fdd0dec6856033cfd3cb019c341712ae5ed9e6e3
Aurametrix <irene@aurametrix.com>
2020-05-07 20:30:25 -0400

    power analysis
    https://towardsdatascience.com/introduction-to-power-analysis-in-python-e7b748dfa26
    need API key for plotly

---
+++ ML/power.py
@@ -0,0 +1,67 @@
+# parameters for the analysis 
+effect_size = 0.8
+alpha = 0.05 # significance level
+power = 0.8
+
+power_analysis = TTestIndPower()
+sample_size = power_analysis.solve_power(effect_size = effect_size, 
+                                         power = power, 
+                                         alpha = alpha)
+
+print('Required sample size: {0:.2f}'.format(sample_size))
+
+
+# power vs. number of observations 
+
+fig = plt.figure()
+ax = fig.add_subplot(2,1,1)
+fig = TTestIndPower().plot_power(dep_var='nobs',
+                                 nobs= np.arange(2, 200),
+                                 effect_size=np.array([0.2, 0.5, 0.8]),
+                                 alpha=0.01,
+                                 ax=ax, title='Power of t-Test' + '\n' + r'$\alpha = 0.01$')
+ax.get_legend().remove()
+ax = fig.add_subplot(2,1,2)
+fig = TTestIndPower().plot_power(dep_var='nobs',
+                                 nobs= np.arange(2, 200),
+                                 effect_size=np.array([0.2, 0.5, 0.8]),
+                                 alpha=0.05,
+                                 ax=ax, title=r'$\alpha = 0.05$') 
+fig.subplots_adjust(top = 1.4)
+
+
+# for this part I assume significance level of 0.05
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+@np.vectorize
+def power_grid(x,y):
+    power = TTestIndPower().solve_power(effect_size = x, 
+                                        nobs1 = y, 
+                                        alpha = 0.05)
+    return power
+
+X,Y = np.meshgrid(np.linspace(0.01, 1, 51), 
+                  np.linspace(10, 1000, 100))
+X = X.T
+Y = Y.T
+
+Z = power_grid(X, Y) # power
+
+data = [go.Surface(x = effect_size, y= Y, z = Z)]
+
+layout = go.Layout(
+    title='Power Analysis',
+    scene = dict(
+                    xaxis = dict(
+                        title='effect size'),
+                    yaxis = dict(
+                        title='number of observations'),
+                    zaxis = dict(
+                        title='power'),)
+)
+
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig, filename='power_analysis')
+
+
