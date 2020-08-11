8153c262ba5ad08a29fcab67b3a653677af3fa99
Aurametrix <irene@aurametrix.com>
2020-08-10 22:01:05 -0400

    sample size
    https://towardsdatascience.com/how-to-use-python-to-figure-out-sample-sizes-for-your-study-871f0a76a19c

---
+++ ML/sample-size.py
@@ -0,0 +1,93 @@
+import math
+import numpy as np
+from scipy import stats
+import pandas as pd
+import seaborn as sns
+
+def z_calc(p1, p2, n1, n2):
+    p_star = (p1*n1 + p2*n2) / (n1 + n2)
+    return (p2 - p1) / math.sqrt(p_star*(1 - p_star)*((1.0 / n1) + (1.0 / n2)))
+    
+def sample_required(p1, p_diff, alpha):
+    if p_diff <= 0:
+        raise ValueError("p_diff must be > 0")
+    n = 1
+    while True:
+        z = z_calc(p1, p1+p_diff, n1=n, n2=n)
+        p = 1 - stats.norm.cdf(z)
+        if p < alpha:
+            break
+        n += 1
+    return n
+    
+    #Map how sample size changes as choice of p1 changes, holding all 
+#else constant.
+
+p1s = [x*.01 for x in range(96)]
+
+data = []
+for p1 in p1s:
+    record = {}
+    record['Probability Difference'] = p_diff
+    record['Sample Size to Detect Difference'] = sample_required(p1=p1,
+                                                                p_diff=.05,
+                                                                alpha=.05)
+    record['Confidence Level'] = '95%'
+    record['Initial Probability'] = p1 * 100
+    data.append(record)
+    
+df = pd.DataFrame(data)
+
+from matplotlib import pyplot
+
+fig, ax = pyplot.subplots(figsize=(9, 9))
+sns.set(style='darkgrid')
+
+plot = sns.pointplot(x='Initial Probability',
+            y='Sample Size to Detect Difference',
+            hue='Confidence Level', ax = ax,
+            data=df)
+
+labels = []
+for i in range(0, 101):
+    if i % 5 == 0:
+        labels.append(str(i))
+    else:
+        labels.append('')
+plot.set_xticklabels(labels=labels)
+
+#Observe probability difference from 2% to 10%
+p_diffs = [x*.01 for x in range(2,11)]
+
+data = []
+for p_diff in p_diffs:
+    record = {}
+    record['Probability Difference'] = p_diff * 100
+    record['Sample Size to Detect Difference'] = sample_required(p1=.5,
+                                                                p_diff=p_diff,
+                                                                alpha=.05)
+    record['Confidence Level'] = '95%'
+    record['Initial probability'] = '50%'
+    data.append(record)
+
+for p_diff in p_diffs:
+    record = {}
+    record['Probability Difference'] = p_diff * 100
+    record['Sample Size to Detect Difference'] = sample_required(p1=.5,
+                                                                p_diff=p_diff,
+                                                                alpha=.01)
+    record['Confidence Level'] = '99%'
+    record['Initial probability'] = '50%'
+    data.append(record)
+
+df = pd.DataFrame(data)
+
+from matplotlib import pyplot
+fig, ax = pyplot.subplots(figsize=(10, 10))
+
+sns.set(style='darkgrid')
+
+plot = sns.pointplot(x='Probability Difference',
+            y='Sample Size to Detect Difference',
+            hue='Confidence Level', ax = ax,
+            data=df)
