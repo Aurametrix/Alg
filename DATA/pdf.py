1d1a2f74acb78792ab3dbd26afe6890e3d46338a
Aurametrix <irene@aurametrix.com>
2020-09-26 20:45:51 -0400

    tabula
    https://pypi.org/project/tabula-py/
    
    Once you have Java, install tabula-py with pip:
    pip install tabula-py

--- DATA/pdf.py
+++ DATA/pdf.py
@@ -4,3 +4,18 @@ pdf_document = PyPDF2.PdfFileReader(mypdf)
 pdf_document.numPages
 first_page = pdf_document.getPage(0)
 print(first_page.extractText())
+
+#### or use tabula
+import tabula
+
+# Read pdf into list of DataFrame
+df = tabula.read_pdf("test.pdf", pages='all')
+
+# Read remote pdf into list of DataFrame
+df2 = tabula.read_pdf("https://github.com/tabulapdf/tabula-java/raw/master/src/test/resources/technology/tabula/arabic.pdf")
+
+# convert PDF into CSV file
+tabula.convert_into("test.pdf", "output.csv", output_format="csv", pages='all')
+
+# convert all PDFs in a directory
+tabula.convert_into_by_batch("input_directory", output_format='csv', pages='all')
